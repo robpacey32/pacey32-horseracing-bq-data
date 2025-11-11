87e2d3f1475929e99be5439782a6dc62debd2267
robpacey32 <143297575+robpacey32@users.noreply.github.com>
2025-11-11 22:32:00 +0000

    Update 1. SportingLife_RaceSpineCreation.py

--- 1. History/1. SportingLife_RaceSpineCreation.py
+++ 1. History/1. SportingLife_RaceSpineCreation.py
@@ -158,8 +158,8 @@ def write_spine_to_bq(df, project_id="horseracing-pacey32-github", dataset="hors
 # 🚀 MAIN EXECUTION
 # ===============================================================
 if __name__ == "__main__":
-    start_date_str = "2025-11-01"
-    end_date_str = "2025-11-01"
+    start_date_str = "2025-01-01"
+    end_date_str = "2025-11-11"
 
     all_races_df = get_races_for_date_range(start_date_str, end_date_str, debug=True)
     write_spine_to_bq(all_races_df)
